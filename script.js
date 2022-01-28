817707057b94ceec69928afb7626a9e715589376
jenishtajale <98461305+jenishtajale@users.noreply.github.com>
2022-01-28 18:27:57 +0545

    Add files via upload
    Final project

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ $(document).ready(function(){
 
     });
 });
+
+var typed= new Typed(".typing",{
+    strings:["Web Developer", "Designer", "Speaker", "Student"],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop: true
+});
+var typed= new Typed(".typing-2",{
+    strings:["Web Developer", "Designer", "Speaker", "Student"],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop: true
+});
